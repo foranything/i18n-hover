fa98d70bdbdb9fd3a4c95fe301121defe7ef6537
Ian.lee <ian.lee@cupix.com>
2024-01-22 22:34:05 +0900

    feat: status bar

---
+++ src/statusBar.ts
@@ -0,0 +1,33 @@
+import * as vscode from 'vscode';
+
+const COMMAND = "vscode-ext-test.toggleReplaceFeature";
+
+const label = {
+  off: `$(x) Replace OFF`,
+  on: `$(check) Replace ON`
+};
+
+let statusBarItem: vscode.StatusBarItem;
+let featureEnabled = false;
+
+export function init(context: vscode.ExtensionContext) {
+  statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
+  statusBarItem.command = COMMAND;
+  statusBarItem.text = label.off;
+  context.subscriptions.push(statusBarItem);
+}
+
+export function updateStatusBarItem(): void {
+  statusBarItem.text = featureEnabled ? label.on : label.off;
+  statusBarItem.show();
+}
+
+export function registerCommands(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand(COMMAND, () => {
+		featureEnabled = !featureEnabled;
+		updateStatusBarItem();
+	});
+
+	context.subscriptions.push(disposable);
+  updateStatusBarItem();
+}
